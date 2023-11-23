2c705f3554446604886d751cd3c60787b37f3963
itsYashASeeker <yashchauhan.coder@gmail.com>
2023-11-23 23:56:36 +0530

    routing error solved 2

--- src/App.jsx
+++ src/App.jsx
@@ -14,7 +14,7 @@ function App() {
     // <>
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Home />} />
+        <Route exact path="/" element={<Home />} />
         <Route path="/info" element={<InfoHome />} />
         <Route path="/info/about" element={<IAbout />} />
       </Routes>
